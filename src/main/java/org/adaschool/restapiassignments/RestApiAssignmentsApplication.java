c2d64dc16ef3a1fc2fd9c960fcbe506fbda0de38
sancarbar <sancabar@gmail.com>
2023-11-19 20:44:05 -0500

    Base project generated with Spring intlzr and with ada client binaries.

---
+++ src/main/java/org/adaschool/restapiassignments/RestApiAssignmentsApplication.java
@@ -0,0 +1,13 @@
+package org.adaschool.restapiassignments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiAssignmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiAssignmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/adaschool/restapiassignments/RestApiAssignmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.adaschool.restapiassignments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiAssignmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
