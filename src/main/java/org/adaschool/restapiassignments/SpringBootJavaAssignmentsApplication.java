bb43c01ffd902cea625bba560c75f57dc5a3087c
Manuel Ernesto Bonilla Muñoz <mebonilla9@gmail.com>
2023-11-21 23:59:55 -0500

    Created unit test for the assignment one

--- src/main/java/org/adaschool/restapiassignments/SpringBootJavaAssignmentsApplication.java
+++ src/main/java/org/adaschool/restapiassignments/SpringBootJavaAssignmentsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestApiAssignmentsApplication {
+public class SpringBootJavaAssignmentsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestApiAssignmentsApplication.class, args);
+		SpringApplication.run(SpringBootJavaAssignmentsApplication.class, args);
 	}
 
 }
